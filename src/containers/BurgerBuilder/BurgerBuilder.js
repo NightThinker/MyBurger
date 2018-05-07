9cecb3eba3558b139bb9921a7e33b640e6c30169
jiraphon <jiraphon@sirimedia.com>
2018-05-07 22:13:34 +0700

    adding to show price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,7 +101,8 @@ class BurgerBuilder extends Component {
           <OrderSumary 
             ingredients={this.state.ingredients}
             purchaseCanceled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler} />
+            purchaseContinued={this.purchaseContinueHandler}
+            price={this.state.totalPrice} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls 
