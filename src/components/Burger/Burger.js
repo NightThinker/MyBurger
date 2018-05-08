6b996c781e7be2ce023eb4de980a965dd995b682
jiraphon <jiraphon@sirimedia.com>
2018-05-08 15:59:40 +0700

    Improving  performance

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
-import Aux from '../../hoc/Aux';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
   let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
-      console.log('igKey: ', igKey);
       return [...Array(props.ingredients[igKey])].map((_, i)=> {
-        console.log('i: ', i);
-        console.log('ingredients: ', props.ingredients[igKey]);
         return <BurgerIngredient key={igKey + i} type={igKey} />
       })
     }).reduce((arr, el) => {
       return arr.concat(el)
     }, []);
-  console.log('transformedIngredients: ', transformedIngredients);
   if(transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients!</p>
   }
--- src/components/Burger/OrderSumary/OrderSumary.js
+++ src/components/Burger/OrderSumary/OrderSumary.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       );
     });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients: </p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p>Continue to Checkout?</p>
-      <p><strong>Total Price : {props.price.toFixed(2)}</strong></p>
-      <Button
-        btnType="Danger"
-        cilcked={props.purchaseCanceled} >CANCEL</Button>
-      <Button 
-        btnType="Success"
-        cilcked={props.purchaseContinued} >CONTINUE</Button>
-    </Aux>
-  );
-};
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients: </p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p>Continue to Checkout?</p>
+        <p><strong>Total Price : {this.props.price.toFixed(2)}</strong></p>
+        <Button
+          btnType="Danger"
+          cilcked={this.props.purchaseCanceled} >CANCEL</Button>
+        <Button 
+          btnType="Success"
+          cilcked={this.props.purchaseContinued} >CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
