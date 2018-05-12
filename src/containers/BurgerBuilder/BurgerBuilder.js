447a3c02d65dd744d4f30688843aede2de21a839
jiraphon <jiraphon@sirimedia.com>
2018-05-12 18:47:13 +0700

    Order Submission & Passing Data Beteen Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -98,35 +98,11 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You Continue!');
-
-    // this.setState({loading: true});
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'May',
-    //     address: {
-    //       street: 'street 1',
-    //       zipCode: '12045',
-    //       country: 'Thailand'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // }
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     // console.log('response: ', response);
-    //     this.setState({loading: false, purchasing: false});
-    //   })
-    //   .catch(error => {
-    //     // console.log('error: ', error);
-    //     this.setState({loading: false, purchasing: false});
-    //   });
     const queryParams = [];
     for(let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&')
 
     this.props.history.push({
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
+import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+
 import classes from './ContactData.css';
 
 class ContactData extends Component {
@@ -10,21 +13,58 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    // console.log(this.props.ingredients);
+
+    this.setState({loading: true});
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: 'May',
+        address: {
+          street: 'street 1',
+          zipCode: '12045',
+          country: 'Thailand'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
     }
+    axios.post('/orders.json', order)
+      .then(response => {
+        // console.log('response: ', response);
+        this.setState({loading: false});
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        // console.log('error: ', error);
+        this.setState({loading: false});
+      });
   }
 
   render() {
-    console.log('contact');
+    let form = (
+      <form>
+        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+        <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+        <input className={classes.Input} type="text" name="street" placeholder="street" />
+        <input className={classes.Input} type="text" name="postal" placeholder="Your postal Code" />
+        <Button btnType="Success" cilcked={this.orderHandler}>ORDER</Button>
+      </form>
+    );
+    if(this.state.loading) {
+      form = <Spinner />
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter Your Contact Data </h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-          <input className={classes.Input} type="text" name="street" placeholder="street" />
-          <input className={classes.Input} type="text" name="postal" placeholder="Your postal Code" />
-          <Button btnType="Success">ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
