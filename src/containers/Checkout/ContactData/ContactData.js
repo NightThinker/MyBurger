f592fb5ca0001a4c90557f2c9f344ce6c5943e01
jiraphon <jiraphon@sirimedia.com>
2018-05-29 23:21:49 +0700

    Adding Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -108,16 +108,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        // console.log('response: ', response);
-        this.setState({loading: false});
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        // console.log('error: ', error);
-        this.setState({loading: false});
-      });
   }
 
   checkValidity(value, rules) {
