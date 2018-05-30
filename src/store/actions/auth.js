49f3ec2e660edbe5d98da982c35345d7c0e05dc0
jiraphon <jiraphon@sirimedia.com>
2018-05-30 17:27:55 +0700

    Getting a Token from he Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,6 +24,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
-    
-  }
-}
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCOLXbHlqTkNLkjMAu-V35lWlM3JdOmkog', authData)
+      .then(response => {
+        console.log('response: ', response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        console.log('error: ', error);
+        dispatch(authFail(error));
+      });
+  };
+};
