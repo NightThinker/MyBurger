1e64ea5d38c516b0c9fe8f39a97caaa71f0b8cf1
jiraphon <jiraphon@sirimedia.com>
2018-06-08 10:41:33 +0700

    Accessing Protected Resources

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,7 +10,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 class Orders extends Component {
 
   componentDidMount() {
-    this.props.onFatchOrders()
+    this.props.onFatchOrders(this.props.token)
   }
 
   render() {
@@ -34,13 +34,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
   return {
     orders: state.order.orders,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   }
 }
 
 const mapDisptchToProps = dispatch => {
   return {
-    onFatchOrders: () => dispatch(actions.fatchOrders())
+    onFatchOrders: (token) => dispatch(actions.fatchOrders(token))
   }
 }
 
