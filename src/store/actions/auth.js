e6af57c82bdfa438be38cede409e5ba11b0847ea
jiraphon <jiraphon@sirimedia.com>
2018-06-17 23:31:28 +0700

    Handling Authentication with a saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -45,30 +45,11 @@ export const checkAuthTimeout = (expirationTime) => {
 }
 
 export const auth = (email, password, isSignup) => {
-  return dispatch => {
-    dispatch(authStart());
-    const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    };
-    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCOLXbHlqTkNLkjMAu-V35lWlM3JdOmkog';
-    if (!isSignup) {
-      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCOLXbHlqTkNLkjMAu-V35lWlM3JdOmkog';
-    }
-    axios.post(url, authData)
-      .then(response => {
-        // console.log('response: ', response.data);
-        const expirationDate =  new Date(new Date().getTime() + response.data.expiresIn * 1000);
-        localStorage.setItem('token', response.data.idToken);
-        localStorage.setItem('expirationDate', expirationDate);
-        localStorage.setItem('userId', response.data.localId);
-        dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.expiresIn));
-      })
-      .catch(error => {
-        dispatch(authFail(error.response.data.error));
-      });
+  return {
+    type: actionTypes.AUTH_USER,
+    email: email,
+    password: password,
+    isSignup: isSignup
   };
 };
 
